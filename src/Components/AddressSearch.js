8e3f09d20b9179232cf755a4793bace62ae979b0
André Merlo <andremmerlo@gmail.com>
2018-01-21 16:33:25 -0200

    adicionado exibição do resultado da consulta conforme CEP digitado

--- src/Components/AddressSearch.js
+++ src/Components/AddressSearch.js
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
 import Panel from './Panel';
+import fetchJsonp from 'fetch-jsonp';
+require('es6-promise').polyfill();
+
+class ErrorMessage extends Component {
+    render() {
+        return (
+            <div className="error-msg">{this.props.message}</div>
+        )
+    }
+}
 
 class AddressInfo extends Component {
     render() {
@@ -20,22 +30,40 @@ class AddressSearchResult extends Component {
     constructor(props) {
         super(props);
     }
-
     render() {
 
-        const {searchResult} = this.props;
+        const searchResult = this.props.searchResult
 
-        let address = {
-            street: searchResult.rua,
-            neighborhood: searchResult.bairro,
-            city: searchResult.cidade,
-            state: searchResult.uf,
-            zipCode: searchResult.cep,
-        };
+        if(!searchResult)
+            return null
+
+        const error = searchResult.erro;
+        let el = null;
+
+        if(error){
+            el = (
+                <div className="address-result-error">
+                    <ErrorMessage message="Endereço não encontrado para o CEP informado."/>
+                </div>
+            )
+        } else {
+            let address = {
+                street: searchResult.logradouro,
+                neighborhood: searchResult.bairro,
+                city: searchResult.localidade,
+                state: searchResult.uf,
+                zipCode: searchResult.cep,
+            }
+            el = (
+                <div className="address-result-success">
+                    <AddressInfo address={address} />
+                </div>
+            )
+        }
 
         return (
             <div className="address-search-result">
-                <AddressInfo address={address}/>
+                {el}
             </div>
         );
     }
@@ -104,23 +132,33 @@ class AddressSearchBar extends Component {
 class AddressSearch extends Component {
     constructor(props) {
         super(props);
+        this.state = { searchResult: undefined }
+        this.handleSearch = this.handleSearch.bind(this);
     }
-
     render() {
-        const searchResult = {
-            rua: 'Rua Miguel Mentem',
-            bairro: 'Vila Guilherme',
-            cidade: 'São Paulo',
-            uf: 'SP',
-            cep: '02050-010',
-        };
         return (
             <Panel title="Consultar Endereço"
                    className="address-search-panel">
-                <AddressSearchBar />
-                <AddressSearchResult searchResult={searchResult}/>
+                <AddressSearchBar
+                    onSubmit={this.handleSearch} />
+                <AddressSearchResult searchResult={this.state.searchResult}/>
             </Panel>
         );
+    }
+    handleSearch(zipCode) {
+
+        const cleanZipCode = zipCode.replace(/\D/g, '');
+
+        fetchJsonp(`https://viacep.com.br/ws/${cleanZipCode}/json/`)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(json) {
+                this.setState({searchResult: json})
+            }.bind(this))
+            .catch(function(ex) {
+                this.setState({searchResult: null})
+            }.bind(this))
     }
 }
 

affa3a92418c92f51806a115be045a30ad044a68
André Merlo <andremmerlo@gmail.com>
2018-01-21 15:52:38 -0200

    adicionado comportamento de preenchimento do CEP e disparo da Busca

--- src/Components/AddressSearch.js
+++ src/Components/AddressSearch.js
@@ -42,25 +42,62 @@ class AddressSearchResult extends Component {
 }
 
 class AddressSearchBar extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             zipcode: '',
             isValid: false,
-        }
+        };
+        this.zipRegex = /^[0-9]{5}-[0-9]{3}$/;
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     render() {
         return (
             <div className="address-search-bar">
                 <p>Consultar</p>
-                <form>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
                     <label>CEP</label>
-                    <input name="zipcode" type="text" placeholder="00000-000"/>
+                    <input name="zipcode" value={this.state.zipcode} type="text" placeholder="00000-000"
+                           onChange={(event) => this.handleChange(event)}/>
                     <button disabled={!this.state.isValid}>Buscar</button>
                 </form>
             </div>
         );
+    }
+
+    handleChange(e) {
+
+        const re = /^[0-9]{1,5}$|^[0-9]{5}-[0-9]{0,3}$/;
+        const prevValue = this.state.zipcode;
+        const value = e.target.value;
+        let finalValue = value;
+
+        //aplica uma máscara enquanto digita
+        if ((prevValue.length < 5 && value.length >= 5)
+            || (prevValue.length === 5 && value.length > 5)) {
+            finalValue = value.substr(0, 5) + '-' + value.substr(5, 3);
+        } else if ((prevValue.length > 6 && value.length === 6)) {
+            finalValue = value.substr(0, 5);
+        }
+
+        if (finalValue === '' || re.test(finalValue)) {
+            this.setState({zipcode: finalValue}, this.validateInput);
+        }
+
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.isValid && this.props.onSubmit) {
+            this.props.onSubmit(this.state.zipcode);
+        }
+    }
+
+    validateInput() {
+        this.setState({isValid: this.zipRegex.test(this.state.zipcode)})
     }
 }
 
